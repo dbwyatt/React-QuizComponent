dfc4c7a210f846e0ed0fe1dcc1181ec9a2c603ba
Daniel Wyatt <dbwyatt23@gmail.com>
2018-06-20 23:30:22 -0600

    feat(end): add functionality for buttons to advance questions to end

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -8,6 +8,12 @@ class QuizQuestion extends Component {
     this.state = {};
   }
 
+  handleClick(buttonText) {
+    if (buttonText === this.props.quiz_question.answer) {
+      this.props.showNextQuestionHandler();
+    }
+  }
+
   render() {
     return (
       <main>
@@ -16,7 +22,9 @@ class QuizQuestion extends Component {
         </section>
         <section className="buttons">
           <ul>
-            <QuizQuestionButton button_text={this.props.quiz_question.answer_options[0]} />
+            {this.props.quiz_question.answer_options.map((answer_option, index) => (
+              <QuizQuestionButton clickHandler={this.handleClick.bind(this)} button_text={answer_option} key={index} />
+            ))}
           </ul>
         </section>
       </main>
